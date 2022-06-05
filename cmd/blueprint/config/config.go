e2518856b172dbd6e7bf54cfa760b641ac7c448f
PabloGolobar <86614673+PabloGolobar@users.noreply.github.com>
2022-06-05 19:58:11 +0300

    First commit

--- cmd/blueprint/config/config.go
+++ cmd/blueprint/config/config.go
@@ -3,18 +3,33 @@ package config
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"gorm.io/gorm"
+	"log"
 )
 
 // Config is global object that holds all application level variables.
 var Config appConfig
 
 type appConfig struct {
-	// Example Variable
-	ConfigVar string
+	// the shared DB ORM object
+	DB *gorm.DB
+	// the error thrown be GORM when using DB ORM object
+	DBErr error
+	// the server port. Defaults to 8080
+	ServerPort int `mapstructure:"server_port"`
+	// the data source name (DSN) for connecting to the database. required.
+	DSN string `mapstructure:"dsn"`
+	// the API key needed to authorize to API. required.
+	//ApiKey string `mapstructure:"api_key"`
+	// Certificate file for HTTPS
+	CertFile string `mapstructure:"cert_file"`
+	// Private key file for HTTPS
+	KeyFile string `mapstructure:"key_file"`
 }
 
 // LoadConfig loads config from files
 func LoadConfig(configPaths ...string) error {
+	log.Println("Loading config...")
 	v := viper.New()
 	v.SetConfigName("example")
 	v.SetConfigType("yaml")
@@ -26,5 +41,12 @@ func LoadConfig(configPaths ...string) error {
 	if err := v.ReadInConfig(); err != nil {
 		return fmt.Errorf("failed to read the configuration file: %s", err)
 	}
+
+	log.Println("Getting DSN...")
+	Config.DSN = v.Get("DSN").(string)
+	log.Println("Got DSN...")
+	//Config.ApiKey = v.Get("API_KEY").(string)
+	v.SetDefault("server_port", 8080)
+	log.Println("Loaded config...")
 	return v.Unmarshal(&Config)
 }
--- cmd/blueprint/main.go
+++ cmd/blueprint/main.go
@@ -2,14 +2,60 @@ package main
 
 import (
 	"fmt"
+	"github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/apis"
 	"github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/config"
+	_ "github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
 )
 
+// @title Blueprint Swagger API
+// @version 1.0
+// @description Swagger API for Golang Project Blueprint.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.email martin7.heinz@gmail.com
+
+// @license.name MIT
+// @license.url https://github.com/MartinHeinz/go-project-blueprint/blob/master/LICENSE
+
+// @BasePath /api/v1
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
-	// load application configurations
 	if err := config.LoadConfig("./config"); err != nil {
 		panic(fmt.Errorf("invalid application configuration: %s", err))
 	}
+	log.Println("Opening db...")
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Opened db")
+	config.Config.DB = db
+	log.Println("Creating router...")
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	log.Println("Created router")
+	log.Println("Creating swagger...")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	log.Println("Created swagger...")
+	log.Println("Creating routes...")
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/users/:id", apis.GetUser)
+	}
+	log.Println("Created routes...")
+	log.Println("Starting server...")
+	r.Run(fmt.Sprintf(":%v", config.Config.ServerPort))
+	// load application configurations
 
-	fmt.Println(config.Config.ConfigVar)
 }
